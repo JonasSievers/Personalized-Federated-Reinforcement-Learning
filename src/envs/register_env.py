3477cca17a5387574cb23d50d3a430d2be95e242
Pascal Henrich <pascalh.1505@gmail.com>
2024-11-18 23:00:09 +0100

    corrected spelling

--- src/envs/register_env.py
+++ src/envs/register_env.py
@@ -2,6 +2,6 @@ from gymnasium.envs.registration import register
 
 register(
     id="EnergyManagementEnv-v0",
-    entry_point="envs.EnergyManagementEvn:EnergyManagementEnv",
+    entry_point="envs.EnergyManagementEnv:EnergyManagementEnv",
     kwargs= {"cfg": None}
 )
