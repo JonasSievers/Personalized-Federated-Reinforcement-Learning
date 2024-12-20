b80ed4ef6f786ff14d55d4967ae8c8253bdfe027
Pascal Henrich <pascalh.1505@gmail.com>
2024-12-20 10:40:06 +0100

    updated price dataset

--- src/utils/EnergyDataset.py
+++ src/utils/EnergyDataset.py
@@ -5,15 +5,19 @@ import pandas as pd
 class EnergyDataset(Dataset):
     def __init__(self, data_path: str, mode: str):
         csv_data = pd.read_csv(data_path, header=0)
+        csv_data['Date'] = pd.to_datetime(csv_data['Date'])
+        csv_data = csv_data[csv_data['Date'].dt.date !=pd.to_datetime('2012-02-29').date()]
+        csv_price = pd.read_csv('../data/price.csv', header=0)
+        csv_data['price'] = csv_price['Price (EUR/MWhe)']
         csv_data.drop(columns=['Date'], inplace=True)
-        csv_data.set_index(pd.RangeIndex(0,52608),inplace=True)
+        csv_data.set_index(pd.RangeIndex(0,52560),inplace=True)
         csv_data.fillna(0, inplace=True)
         if mode == 'train':
             csv_data = csv_data.loc[0:17519]
         elif mode == 'eval':
-            csv_data = (csv_data.loc[17520:35087]).set_index(pd.RangeIndex(0,17568))
+            csv_data = (csv_data.loc[17520:35039]).set_index(pd.RangeIndex(0,17520))
         elif mode == 'test':
-            csv_data = (csv_data.loc[35088:52607]).set_index(pd.RangeIndex(0,17520))
+            csv_data = (csv_data.loc[35040:52559]).set_index(pd.RangeIndex(0,17520))
         self._data = pd.DataFrame({
             f'net_load_{i+1}': csv_data[f'load_{i+1}'] - csv_data[f'pv_{i+1}'] 
             for i in range(300)})
